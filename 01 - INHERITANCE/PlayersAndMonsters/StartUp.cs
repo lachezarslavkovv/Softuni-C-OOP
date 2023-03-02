1ff0891aa7b6db186086d34a7fa0e0a6d9501712
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-03-02 06:28:48 +0200

    Add files via upload

---
+++ 01 - INHERITANCE/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+        }
+
+    }
+}
