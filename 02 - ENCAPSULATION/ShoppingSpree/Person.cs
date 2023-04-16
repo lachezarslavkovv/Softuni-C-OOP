f92d5d87665019f36dc58e63d76de4f6c04375e2
lachezarslavkovv <106038404+lachezarslavkovv@users.noreply.github.com>
2023-04-16 04:54:38 +0300

    Add files via upload

---
+++ 02 - ENCAPSULATION/ShoppingSpree/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            products = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> Products 
+        {
+            get => this.products;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (this.Money-product.Cost<0)
+            {
+                return false;
+            }
+            else
+            {
+                products.Add(product);
+                this.Money -= product.Cost;
+                return true;
+            }
+        }
+
+    }
+}
